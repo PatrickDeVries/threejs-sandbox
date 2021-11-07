188fb5c220cce1e3ed50b2deaec2d35a36b51c57
Patrick DeVries <pcdevri@gmail.com>
2021-11-06 23:27:22 -0500

    Positions saved when changing controls

--- components/FiberLayout.tsx
+++ components/FiberLayout.tsx
@@ -3,8 +3,9 @@ import { Canvas, render, useFrame, useThree } from '@react-three/fiber';
 import styled from 'styled-components';
 import { AdditiveBlending, BufferAttribute, BufferGeometry, Points, TextureLoader } from 'three';
 import * as THREE from 'three';
-import HeatMap from 'heatmap-ts';
-import { Button, RangeSlider, Text, Card, Label } from '@headstorm/foundry-react-ui';
+import { Button, RangeSlider, Text, Card, Label, TextInput } from '@headstorm/foundry-react-ui';
+import Icon from '@mdi/react';
+import { mdiChevronDown, mdiChevronUp } from '@mdi/js';
 
 const BgCanvas = styled.div`
   position: fixed;
@@ -12,7 +13,7 @@ const BgCanvas = styled.div`
   left: 0;
   height: 100vh;
   width: 100vw;
-  background-image: linear-gradient(168deg, #ffffff, #999);
+  background-image: linear-gradient(168deg, #fff, #555);
 `;
 
 const Overlay = styled.div`
@@ -25,9 +26,12 @@ const Overlay = styled.div`
 `;
 
 const ControlCard = styled(Card.Container)`
-  width: 30vw;
+  ${({ visibleControls }: { visibleControls: boolean }) =>
+    visibleControls ? 'width: 30vw;' : 'max-width: 30vw;'}
   margin: 1rem;
-  background-color: #fff5;
+  background-color: #fff8;
+  max-height: 100vh;
+  transition: all 5s;
 `;
 
 const visibleHeightAtZDepth = (depth, camera) => {
@@ -50,77 +54,35 @@ const visibleWidthAtZDepth = (depth, camera) => {
 
 const Particles = props => {
   const viewport = useThree(state => state.viewport);
-  // const camera = useThree(state => state.camera);
-
-  // var heatMap = [];
-  // for (let i = 0; i < viewport.height; i++) {
-  //   for (let j = 0; j < viewport.width; j++) {
-  //     heatMap.push({ x: j * 100, y: i * 100, value: 0, radius: 30 });
-  //   }
-  // }
-  // console.table(heatMap);
-  // const renderMap = new HeatMap({
-  //   container: document.getElementById('bgCanvas'),
-  //   maxOpacity: 0.6,
-  //   radius: 1,
-  //   blur: 0.9,
-  // });
-  const particles = useRef<BufferGeometry>();
+  const particles = props.particles;
   const pointRef = useRef<Points>();
   const particleCount = props.particleCount;
   const baseV = props.baseV;
   const vVariance = props.vVar;
   const baseTurnSpeed = props.baseTurnV;
   const turnVariance = props.turnVar;
-  const positions = [];
-  const velocities = [];
-  const angles = [];
-  for (let i = 0; i < 999999; i++) {
-    positions.push(
-      Math.random() * viewport.width - viewport.width / 2,
-      Math.random() * viewport.height - viewport.height / 2,
-      0,
-    );
-    velocities.push(Math.random() * vVariance, Math.random() * turnVariance, 0);
-    angles.push(Math.random() * 2 * Math.PI);
+
+  if (props.positions.length < particleCount) {
+    console.log('set');
+    const positions = [];
+    const velocities = [];
+    const angles = [];
+    for (let i = 0; i < 999999; i++) {
+      positions.push(
+        Math.random() * viewport.width - viewport.width / 2,
+        Math.random() * viewport.height - viewport.height / 2,
+        0,
+      );
+      velocities.push(Math.random(), Math.random(), 0);
+      angles.push(Math.random() * 2 * Math.PI);
+    }
+
+    props.setPositions(positions);
+    props.setVelocities(velocities);
+    props.setAngles(angles);
   }
 
   const pi2 = Math.PI * 2;
-
-  // const isOccupied = (i: number, j: number, pps: BufferAttribute) => {
-  //   for (let k = 0; k < particleCount; k++) {
-  //     let position = new THREE.Vector3(pps.getX(k), pps.getY(k), pps.getZ(k));
-  //     position.project(camera);
-  //     let pixelX = Math.round((position.x * viewport.width) / 2 + viewport.width / 2);
-  //     let pixelY = Math.round(-(position.y * viewport.height) / 2 + viewport.height / 2);
-
-  //     if (pixelX === j && pixelY === i) {
-  //       return true;
-  //     }
-  //   }
-  //   return false;
-  // };
-
-  // let count = 0;
-
-  // const updateHeatMap = () => {
-  //   const pps: BufferAttribute = particles.current['attributes']['position'] as BufferAttribute;
-
-  //   if (count === 0) {
-  //     console.log(heatMap);
-  //     console.table(heatMap);
-  //     count += 1;
-  //   }
-  //   heatMap.forEach((cell, i) => {
-  //     if (isOccupied(cell.y / 10, cell.x / 10, pps)) {
-  //       heatMap[i].value += 1;
-  //     } else if (heatMap[i].value > 0) {
-  //       heatMap[i].value -= 1;
-  //     }
-  //   });
-
-  //   renderMap.setData({ max: 100, min: 0, data: heatMap });
-  // };
 
   const updatePositions = () => {
     if (particles.current) {
@@ -130,8 +92,8 @@ const Particles = props => {
 
       for (let i = 0, l = particleCount; i < l; i++) {
         let angle = pas.getX(i);
-        let v = pvs.getX(i) + baseV;
-        let turnV = pvs.getY(i) + baseTurnSpeed;
+        let v = pvs.getX(i) * vVariance + baseV;
+        let turnV = pvs.getY(i) * turnVariance + baseTurnSpeed;
 
         pps.setXY(i, pps.getX(i) + v * Math.cos(angle), pps.getY(i) + v * Math.sin(angle));
 
@@ -167,26 +129,26 @@ const Particles = props => {
         <bufferAttribute
           attachObject={['attributes', 'position']}
           count={particleCount}
-          array={new Float32Array(positions)}
+          array={new Float32Array(props.positions)}
           itemSize={3}
         />
         <bufferAttribute
           attachObject={['attributes', 'velocity']}
           count={particleCount}
-          array={new Float32Array(velocities)}
+          array={new Float32Array(props.velocities)}
           itemSize={3}
         />
         <bufferAttribute
           attachObject={['attributes', 'angle']}
           count={particleCount}
-          array={new Float32Array(angles)}
+          array={new Float32Array(props.angles)}
           itemSize={1}
         />
       </bufferGeometry>
       <pointsMaterial
         attach="material"
         size={0.1}
-        color="#0000ff"
+        color={props.color}
         map={new TextureLoader().load('/particle.png')}
         blending={AdditiveBlending}
       />
@@ -195,12 +157,28 @@ const Particles = props => {
 };
 
 const FiberLayout = () => {
+  const [visibleControls, setVibisibleControls] = React.useState<boolean>(true);
+
   const [particleCount, setParticleCount] = React.useState<number>(5000);
   const [baseV, setBaseV] = React.useState<number>(0.05);
   const [vVar, setVVar] = React.useState<number>(0.003);
   const [baseTurnV, setBaseTurnV] = React.useState<number>(0.02 * Math.PI);
   const [turnVar, setTurnVar] = React.useState<number>(0.002 * Math.PI);
   const [freeRate, setFreeRate] = React.useState<number>(200);
+  const [color, setColor] = React.useState<string>('#114455');
+
+  const [positions, setPositions] = React.useState<number[]>([]);
+  const [velocities, setVelocities] = React.useState<number[]>([]);
+  const [angles, setAngles] = React.useState<number[]>([]);
+  const particles = useRef<BufferGeometry>();
+
+  const updateState = () => {
+    if (particles.current) {
+      setPositions(particles.current.attributes.position.array as number[]);
+      setVelocities(particles.current.attributes.velocity.array as number[]);
+      setAngles(particles.current.attributes.angle.array as number[]);
+    }
+  };
 
   return (
     <BgCanvas id="bgCanvas">
@@ -217,82 +195,134 @@ const FiberLayout = () => {
           baseTurnV={baseTurnV}
           turnVar={turnVar}
           freeRate={freeRate}
+          positions={positions}
+          setPositions={setPositions}
+          velocities={velocities}
+          setVelocities={setVelocities}
+          angles={angles}
+          setAngles={setAngles}
+          particles={particles}
+          color={color}
         />
       </Canvas>
       <Overlay>
-        <Card header={<Text>Controls</Text>} StyledContainer={ControlCard}>
-          <Label labelText="Particle count" color="black">
-            <RangeSlider
-              min={1}
-              max={99999}
-              values={[{ value: particleCount, label: particleCount }]}
-              showDomainLabels
-              showHandleLabels
-              onDrag={(newVal: number) => {
-                setParticleCount(Math.round(newVal));
+        <Card
+          header={
+            <Button
+              color="#fff2"
+              onClick={() => {
+                updateState();
+                setVibisibleControls(!visibleControls);
               }}
-            />
-          </Label>
-          <Label labelText="Base Velocity" color="black">
-            <RangeSlider
-              min={0}
-              max={1}
-              values={[{ value: baseV, label: parseFloat(baseV.toFixed(4)) }]}
-              showDomainLabels
-              showHandleLabels
-              onDrag={(newVal: number) => {
-                setBaseV(newVal);
-              }}
-            />
-          </Label>
-          <Label labelText="Velocity Variance" color="black">
-            <RangeSlider
-              min={0}
-              max={1}
-              values={[{ value: vVar, label: parseFloat(vVar.toFixed(4)) }]}
-              showDomainLabels
-              showHandleLabels
-              onDrag={(newVal: number) => {
-                setVVar(newVal);
-              }}
-            />
-          </Label>
-          <Label labelText="Base Turn Speed" color="black">
-            <RangeSlider
-              min={0}
-              max={Math.PI / 4}
-              values={[{ value: baseTurnV, label: parseFloat(baseTurnV.toFixed(4)) }]}
-              showDomainLabels
-              showHandleLabels
-              onDrag={(newVal: number) => {
-                setBaseTurnV(newVal);
-              }}
-            />
-          </Label>
-          <Label labelText="Turn Speed Variance" color="black">
-            <RangeSlider
-              min={0}
-              max={Math.PI / 4}
-              values={[{ value: turnVar, label: parseFloat(turnVar.toFixed(4)) }]}
-              showDomainLabels
-              showHandleLabels
-              onDrag={(newVal: number) => {
-                setTurnVar(newVal);
-              }}
-            />
-          </Label>
-          <Label labelText="Free Thinker Rate (1 per x)" color="black">
-            <RangeSlider
-              min={0}
-              max={particleCount}
-              values={[{ value: freeRate, label: freeRate }]}
-              showDomainLabels
-              showHandleLabels
-              onDrag={(newVal: number) => {
-                setFreeRate(Math.round(newVal));
-              }}
-            />
-          </Label>
+            >
+              <span>
+                <Text>Controls</Text>
+                <Icon size="1rem" path={visibleControls ? mdiChevronDown : mdiChevronUp}></Icon>
+              </span>
+            </Button>
+          }
+          StyledContainer={ControlCard}
+          containerProps={{ visibleControls }}
+        >
+          {visibleControls && (
+            <>
+              <Label labelText="Particle count" color="black">
+                <RangeSlider
+                  min={1}
+                  max={99999}
+                  values={[{ value: particleCount, label: particleCount }]}
+                  showDomainLabels
+                  showHandleLabels
+                  onDrag={(newVal: number) => {
+                    updateState();
+                    setParticleCount(Math.round(newVal));
+                  }}
+                />
+              </Label>
+              <Label labelText="Base Velocity" color="black">
+                <RangeSlider
+                  min={0}
+                  max={1}
+                  values={[{ value: baseV, label: parseFloat(baseV.toFixed(4)) }]}
+                  showDomainLabels
+                  showHandleLabels
+                  onDrag={(newVal: number) => {
+                    updateState();
+                    setBaseV(newVal);
+                  }}
+                />
+              </Label>
+              <Label labelText="Velocity Variance" color="black">
+                <RangeSlider
+                  min={0}
+                  max={1}
+                  values={[{ value: vVar, label: parseFloat(vVar.toFixed(4)) }]}
+                  showDomainLabels
+                  showHandleLabels
+                  onDrag={(newVal: number) => {
+                    updateState();
+                    setVVar(newVal);
+                  }}
+                />
+              </Label>
+              <Label labelText="Base Turn Speed" color="black">
+                <RangeSlider
+                  min={0}
+                  max={Math.PI / 4}
+                  values={[{ value: baseTurnV, label: parseFloat(baseTurnV.toFixed(4)) }]}
+                  showDomainLabels
+                  showHandleLabels
+                  onDrag={(newVal: number) => {
+                    updateState();
+                    setBaseTurnV(newVal);
+                  }}
+                />
+              </Label>
+              <Label labelText="Turn Speed Variance" color="black">
+                <RangeSlider
+                  min={0}
+                  max={Math.PI / 4}
+                  values={[{ value: turnVar, label: parseFloat(turnVar.toFixed(4)) }]}
+                  showDomainLabels
+                  showHandleLabels
+                  onDrag={(newVal: number) => {
+                    updateState();
+                    setTurnVar(newVal);
+                  }}
+                />
+              </Label>
+              <Label labelText="Free Thinker Rate (1 per x)" color="black">
+                <RangeSlider
+                  min={0}
+                  max={particleCount}
+                  values={[{ value: freeRate, label: freeRate }]}
+                  showDomainLabels
+                  showHandleLabels
+                  onDrag={(newVal: number) => {
+                    updateState();
+                    setFreeRate(Math.round(newVal));
+                  }}
+                />
+              </Label>
+              <Label labelText="Color" color="black">
+                <TextInput
+                  maxLength={7}
+                  value={color}
+                  onChange={event => setColor(event.target.value)}
+                />
+              </Label>
+              <Button
+                color="#fff7"
+                onClick={() => {
+                  setPositions([]);
+                  setVelocities([]);
+                  setAngles([]);
+                }}
+              >
+                Reset Particles
+              </Button>
+            </>
+          )}
         </Card>
       </Overlay>
     </BgCanvas>
